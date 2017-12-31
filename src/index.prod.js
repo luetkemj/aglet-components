b2fbec0fd394011891f7691b6843a07bb0880682
Mark Luetke <luetkemj@gmail.com>
2017-12-31 13:37:35 -0600

    export input components from index.prod

--- src/index.prod.js
+++ src/index.prod.js
@@ -1,2 +1,6 @@
 export Header from './components/header/header.component';
 export Footer from './components/footer/footer.component';
+
+/* eslint-disable import/no-named-as-default */
+export InputNumber from './components/input-number/input-number.component';
+export InputText from './components/input-text/input-text.component';
