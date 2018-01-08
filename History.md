e58ca7d24bd0fd7f4a167878ede3531bac3b10dd
Mark Luetke <luetkemj@gmail.com>
2018-01-07 18:08:52 -0600

    v1.3.3

--- History.md
+++ History.md
@@ -1,4 +1,17 @@
 
+v1.3.3 / 2018-01-07
+==================
+
+  * Checkbox component
+  * NumberInput component
+  * TextInput component
+  * HOC Decorators for controlled form elements
+  * add Storyshots
+  * upgrade dependencies
+  * build static Storybook
+  * add Travis CI
+  * add Storybook
+
 v1.3.2 / 2017-08-28
 ==================
 
@@ -13,7 +26,7 @@ v1.3.2 / 2017-08-28
 
 1.0.0 / 2017-08-08
 ==================
-  
+
   * header.component
   * Move assets to CDN
   * Fix production webpack config using 'libraryTarget'
--- src/index.prod.js
+++ src/index.prod.js
@@ -2,5 +2,6 @@ export Header from './components/header/header.component';
 export Footer from './components/footer/footer.component';
 
 /* eslint-disable import/no-named-as-default */
-export InputNumber from './components/input-number/input-number.component';
-export InputText from './components/input-text/input-text.component';
+export Checkbox from './components/input-checkbox/input-checkbox.component';
+export NumberInput from './components/input-number/input-number.component';
+export TextInput from './components/input-text/input-text.component';
