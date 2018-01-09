44528ee1f259f2ada804a248dd2707eca59f9654
Mark Luetke <luetkemj@gmail.com>
2018-01-08 18:34:46 -0600

    update the stories

--- src/components/input-number/input-number.stories.js
+++ src/components/input-number/input-number.stories.js
@@ -14,21 +14,21 @@ storiesOf('Forms/InputNumber', module)
   })(() => (
     <InputNumber
       name="inputNumber"
-      value="0"
+      value={0}
       handleChange={() => {}}
     />
   )))
   .add('Placeholder', withInfo('With placeholder')(() => (
     <InputNumber
       name="inputNumber"
-      placeholder="0"
+      placeholder={0}
       handleChange={() => {}}
     />
   )))
   .add('Label', withInfo('With Label')(() => (
     <InputNumber
       name="inputNumber"
-      value="0"
+      value={0}
       handleChange={() => {}}
       label="Number Input Field"
     />
@@ -36,15 +36,14 @@ storiesOf('Forms/InputNumber', module)
   .add('Required', withInfo('A required InputNumber')(() => (
     <InputNumber
       name="inputNumber"
-      value="0"
+      value={0}
       handleChange={() => {}}
       label="Number Input Field"
       required
     />
   )))
-  .add('Controlled InputNumber', withInfo(`
-    A controlled InputNumber - this component has been wrapped in a Higher Order Component before
-    export to manage state automagically.`)(() => (
+  .add('Controlled', withInfo(`
+    This number input is controlled`)(() => (
       <ControlledInputNumber
         name="inputNumber"
         onChange={() => {}}
@@ -52,14 +51,47 @@ storiesOf('Forms/InputNumber', module)
         required
       />
   )))
-  .add('Controlled InputNumber with initialValue', withInfo(`
-    A controlled InputNumber - this component has been wrapped in a Higher Order Component before
-    export to manage state automagically.`)(() => (
+  .add('Controlled with initialValue', withInfo(`
+    Controlled with an initial value of 10`)(() => (
       <ControlledInputNumber
         name="inputNumber"
-        initialValue="10"
+        initialValue={10}
         onChange={() => {}}
         label="Number Input Field"
         required
+      />
+  )))
+  .add('With min value', withInfo(`
+    Min of 0`)(() => (
+      <ControlledInputNumber
+        name="inputNumber"
+        initialValue={10}
+        onChange={() => {}}
+        label="Number Input Field"
+        required
+        min={0}
+      />
+  )))
+  .add('With max value', withInfo(`
+    Max of 10`)(() => (
+      <ControlledInputNumber
+        name="inputNumber"
+        initialValue={10}
+        onChange={() => {}}
+        label="Number Input Field"
+        required
+        max={10}
+      />
+  )))
+  .add('With min / max value', withInfo(`
+    Min of 0, max of 10`)(() => (
+      <ControlledInputNumber
+        name="inputNumber"
+        initialValue={10}
+        onChange={() => {}}
+        label="Number Input Field"
+        required
+        min={0}
+        max={10}
       />
   )));
