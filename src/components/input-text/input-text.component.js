9e994c67b1f0131df0e70c30eacc9e0eb4cd11d8
Mark Luetke <luetkemj@gmail.com>
2018-03-18 17:07:01 -0500

    update inputText
    - remove decorator in favor of recompose
    - add knobs and remove unecessary stories
    - add readonly and password boolean props
    - add className hook
    - remove initial value in favor of just value
    - getValue callback instead of onChange

--- src/components/input-text/input-text.component.js
+++ src/components/input-text/input-text.component.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import ControlledText from '../decorators/controlled-text.decorator';
+import { compose, setDisplayName, withState, withHandlers } from 'recompose';
 
 import style from './input-text.component.scss';
 
-export function InputText({
+const enhance = compose(
+  setDisplayName('InputText'),
+  withState('value', 'updateValue', ({ value }) => value),
+  withHandlers({
+    onChange: props => (event) => {
+      props.updateValue(event.target.value);
+      props.getValue(event.target.value);
+    },
+  }),
+);
+
+const InputText = enhance(({
+  className,
   label,
   name,
+  onChange,
+  password,
   placeholder,
+  readOnly,
   required,
   value,
-  handleChange,
-}) {
+}) => {
   let isRequired;
   if (required) {
     isRequired = (
@@ -20,36 +34,52 @@ export function InputText({
   }
   return (
     <label
-      className={style.wrapper}
+      className={`${style.wrapper} ${className}`}
       htmlFor={name}
     >
       <p className={style.label}>{label} {isRequired}</p>
       <input
         className={style.input}
         name={name}
-        type="text"
+        type={password ? 'password' : 'text'}
+        readOnly={readOnly}
         value={value}
         placeholder={placeholder}
-        onChange={event => handleChange(event)}
+        onChange={onChange}
       />
     </label>
   );
-}
+});
 
 InputText.propTypes = {
+  /** Optional className hook for container - wraps entire component */
+  className: PropTypes.string,
+  /** Callback for accessing the internal state that tracks the form value */
+  getValue: PropTypes.func,
+  /** Label to display above the input field */
   label: PropTypes.string,
+  /** Used with the htmlFor attribute. Should be unique to form */
   name: PropTypes.string.isRequired,
+  /** Text to display when no user input exists */
   placeholder: PropTypes.string,
+  /** Boolean to toggle password masking */
+  password: PropTypes.bool,
+  /** String for setting readOnly mode */
+  readOnly: PropTypes.bool,
+  /** Boolean to toggle wether the input is required or not */
   required: PropTypes.bool,
+  /** String used to set initial value */
   value: PropTypes.string,
-  handleChange: PropTypes.func.isRequired,
 };
 
 InputText.defaultProps = {
+  className: null,
   label: null,
+  password: false,
   placeholder: null,
+  readOnly: false,
   required: false,
   value: '',
 };
 
-export default ControlledText(InputText);
+export default InputText;
--- src/components/input-text/input-text.stories.js
+++ src/components/input-text/input-text.stories.js
@@ -1,67 +1,27 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { withInfo } from '@storybook/addon-info';
+import { withKnobs, boolean, text } from '@storybook/addon-knobs';
 
 import { StoryWrapper } from '../../../.storybook/storybook-decorators/story-wrapper';
 
-import ControlledInputText, { InputText } from './input-text.component';
+import InputText from './input-text.component';
 
 storiesOf('Forms/InputText', module)
   .addDecorator(StoryWrapper)
+  .addDecorator(withKnobs)
   .add('Basic', withInfo({
     text: 'Minimum required props',
     propTables: [InputText],
   })(() => (
     <InputText
-      name="inputText"
-      value="A static value"
-      handleChange={() => {}}
+      name={text('name', 'name')}
+      getValue={action('getValue')}
+      password={boolean('password', false)}
+      placeholder={text('placeholder', 'placeholder')}
+      readOnly={boolean('readOnly', false)}
+      label={text('label', 'label')}
+      required={boolean('required', true)}
     />
-  )))
-  .add('Placeholder', withInfo('With placeholder')(() => (
-    <InputText
-      name="inputText"
-      handleChange={() => {}}
-      placeholder="this is placeholder text..."
-    />
-  )))
-  .add('Label', withInfo('With Label')(() => (
-    <InputText
-      name="inputText"
-      handleChange={() => {}}
-      value="This field has a label"
-      label="Text Input"
-    />
-  )))
-  .add('Required', withInfo('A required InputText')(() => (
-    <InputText
-      name="inputText"
-      handleChange={() => {}}
-      value="This field is required"
-      label="Text Input"
-      required
-    />
-  )))
-  .add('Controlled InputText', withInfo(`
-    A controlled InputText - this component has been wrapped in a Higher Order Component before
-    export to manage state automagically.`)(() => (
-      <ControlledInputText
-        name="inputText"
-        onChange={() => {}}
-        placeholder="enter text here..."
-        label="Text Input"
-        required
-      />
-  )))
-  .add('Controlled InputText with initial Value', withInfo(`
-    A controlled InputText - this component has been wrapped in a Higher Order Component before
-    export to manage state automagically.`)(() => (
-      <ControlledInputText
-        name="inputText"
-        onChange={val => console.log(val)}
-        placeholder="enter text here..."
-        label="Text Input"
-        required
-        initialValue="This is an initial value. It can be changed."
-      />
   )));
