e0aa18baf7d2f6d8742b73805f364cc5f34e59d6
Mark Luetke <luetkemj@gmail.com>
2018-03-18 16:45:15 -0500

    remove storyshots
    not useful

--- src/test/Storyshots.test.js
+++
@@ -1,7 +0,0 @@
-import initStoryshots from '@storybook/addon-storyshots';
-
-const addonInfo = require('@storybook/addon-info');
-
-addonInfo.withInfo = () => storyFn => storyFn;
-
-initStoryshots();
